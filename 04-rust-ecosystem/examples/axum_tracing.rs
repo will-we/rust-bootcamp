37e8039b4c856842f382cfde572f9cbeb7638c7f
will-we <weirson.lee@gmail.com>
2025-06-18 07:35:25 +0800

    4.3 日志处理接入opentelemetry

--- 04-rust-ecosystem/examples/axum_tracing.rs
+++ 04-rust-ecosystem/examples/axum_tracing.rs
@@ -3,6 +3,10 @@ use axum::routing::get;
 use axum::Router;
 use serde::Deserialize;
 use std::path::PathBuf;
+use opentelemetry::KeyValue;
+use opentelemetry_otlp::WithExportConfig;
+use opentelemetry_sdk::{runtime, trace, Resource};
+use opentelemetry_sdk::trace::{RandomIdGenerator, Tracer};
 use tokio::net::TcpListener;
 use tokio::time::Instant;
 use tracing::level_filters::LevelFilter;
@@ -46,6 +50,10 @@ async fn main() -> anyhow::Result<()> {
             .expect("initializing rolling file appender failed");
     }
 
+    // opentelemetry tracing layer for tracing-subscriber
+    let tracer = init_tracer()?;
+    let opentelemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+
     // let file_appender = tracing_appender::rolling::daily(log_dir, "app.log");
     let file_appender = daily_with_custom_format(log_dir);
     let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
@@ -57,6 +65,7 @@ async fn main() -> anyhow::Result<()> {
     tracing_subscriber::registry()
         .with(console)
         .with(file)
+        .with(opentelemetry)
         .init();
 
     let addr = "0.0.0.0:8080";
@@ -84,4 +93,26 @@ async fn query_handler(Query(params): Query<Params>) -> String {
         "query_handler is took {:.2} seconds", elapsed_s
     );
     return msg;
+}
+
+fn init_tracer() -> anyhow::Result<Tracer> {
+    let tracer = opentelemetry_otlp::new_pipeline()
+        .tracing()
+        .with_exporter(
+            opentelemetry_otlp::new_exporter()
+                .tonic()
+                .with_endpoint("http://localhost:4317"),
+        )
+        .with_trace_config(
+            trace::config()
+                .with_id_generator(RandomIdGenerator::default())
+                .with_max_events_per_span(32)
+                .with_max_attributes_per_span(64)
+                .with_resource(Resource::new(vec![KeyValue::new(
+                    "service.name",
+                    "axum-tracing",
+                )])),
+        )
+        .install_batch(runtime::Tokio)?;
+    Ok(tracer)
 }
